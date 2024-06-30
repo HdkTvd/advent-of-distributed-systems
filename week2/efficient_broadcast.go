afcc922b49bda6e74c1bba12481cf39a649ceec5
hdktvd <hardiktrivedi974@gmail.com>
2024-06-30 23:23:35 +0530

    refactor code

--- week2/efficient_broadcast.go
+++ week2/efficient_broadcast.go
@@ -2,87 +2,16 @@ package week2
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"os"
 	"reflect"
 	"sync"
-	"time"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
-type job struct {
-	Src   string
-	Dest  string
-	Value any
-}
-
-type persistentQueue struct {
-	mu     sync.Mutex
-	ackMap map[job]bool
-}
-
-func newPersistentQueue() *persistentQueue {
-	return &persistentQueue{
-		ackMap: make(map[job]bool),
-	}
-}
-
-func (pq *persistentQueue) markAcked(j job) {
-	pq.mu.Lock()
-	pq.ackMap[j] = true
-	pq.mu.Unlock()
-}
-
-func (pq *persistentQueue) isAcked(j job) bool {
-	pq.mu.Lock()
-	defer pq.mu.Unlock()
-	return pq.ackMap[j]
-}
-
-func consumeJobChannel(jobChan chan job, pq *persistentQueue, n *maelstrom.Node) {
-	log.Printf("Started job queue")
-
-	factor := 50
-
-	for j := range jobChan {
-		go func(jb job) {
-			body := map[string]any{}
-			body["type"] = "broadcast"
-			body["message"] = jb.Value
-
-			attempt := 0
-			for {
-				if err := n.RPC(jb.Dest, body, func(msg maelstrom.Message) error {
-					resBody := map[string]any{}
-					if err := json.Unmarshal(msg.Body, &resBody); err != nil {
-						return err
-					}
-					typ := reflect.ValueOf(resBody["type"]).String()
-					if typ != "broadcast_ok" {
-						return errors.New("broadcast not ok")
-					}
-
-					fmt.Fprintf(os.Stderr, "Acknowledged msg - %v, dest - %v \n", jb.Value, jb.Dest)
-					pq.markAcked(jb)
-
-					return nil
-				}); err != nil || !pq.isAcked(jb) {
-					attempt++
-					fmt.Fprintf(os.Stderr, "Retry %v after err %v, attempt %d\n", jb, "retrying because not acknowledged", attempt)
-					time.Sleep(time.Duration(attempt*factor) * time.Millisecond)
-					continue
-				}
-
-				break
-			}
-		}(j)
-	}
-}
-
-func Fault_tolerant_broadcast() {
+func Efficient_broadcast() {
 	n := maelstrom.NewNode()
 
 	mu := &sync.Mutex{}
